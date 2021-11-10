7fc8c8e3ab68e7715e7410875fc8d471a8aff334
jetsaus <phpsaus@gmail.com>
2021-11-11 00:27:35 +0800

    1.Условие if готово, PhpStorm ругается на 7-ю строку if ($a>=0 && $b>=0) в файле if.php

---
+++ if.php
@@ -0,0 +1,25 @@
+<?php
+    $title = 'Условия';
+    $h4 = 'Оператор if';
+    $a = 1;
+    $b = -1;
+    $strResult = '';                // строка результата
+    if ($a>=0 && $b>=0) {           // $a и $b положительные
+        $strResult = '$a и $b положительные, их разность = ' . ($a-$b);
+    } elseif ($a<0 && $b<0) {       // $a и $b отрицательные
+        $strResult = '$a и $b отрицательные, их произведение = ' . ($a*$b);
+    } elseif (($a>=0 && $b<0) || ($a<0 && $b>=0))  {
+        $strResult = '$a и $b разных знаков, их сумма = ' . ($a-$b);
+    }
+?>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title><?= $title; ?></title>
+</head>
+<body>
+    <h4><?= $h4; ?></h4>
+    <span>Заданы переменные: <?= "\$a = $a, \$b = $b. "; ?></span>
+    <span><?= $strResult; ?></span>
+</body>
+</html>
