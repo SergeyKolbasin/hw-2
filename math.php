67ba417b93faacc5390ee652d25145ab09d46a30
jetsaus <jetsaus@gmail.com>
2022-03-28 23:22:58 +0800

    4.Доработан п.4 ДЗ в соответствии с лекцией

--- math.php
+++ math.php
@@ -3,26 +3,49 @@
  * Основные математические функции
  * Все функции возвращают строковый эквивалет результата выполнения операции
  */
-    // Функция сложения
-    function addFunction($arg1 = 0, $arg2 = 0)
-    {
-        return (string)($arg1 + $arg2);
+/**
+ * Функция сложения аргументов
+ * @param   $arg1   number  аргумент мат.операции
+ * @param   $arg2   number  аргумент мат.операции
+ * @return          string  результат мат.операции
+ */
+function addFunction($arg1 = 0, $arg2 = 0)
+{
+    return (string)($arg1 + $arg2);
+}
+
+/**
+ * Функция вычитания аргументов
+ * @param   $arg1   number  аргумент мат.операции
+ * @param   $arg2   number  аргумент мат.операции
+ * @return          string  результат мат.операции
+ */
+function subFunction($arg1 = 0, $arg2 = 0)
+{
+    return (string)($arg1 - $arg2);
+}
+
+/**
+ * Функция умножения аргументов
+ * @param   $arg1   number  аргумент мат.операции
+ * @param   $arg2   number  аргумент мат.операции
+ * @return          string  результат мат.операции
+ */
+function mulFunction($arg1 = 0, $arg2 = 0)
+{
+    return (string)($arg1 * $arg2);
+}
+
+/**
+ * Функция деления аргументов
+ * @param   $arg1   number  аргумент мат.операции
+ * @param   $arg2   number  аргумент мат.операции
+ * @return          string  результат мат.операции
+ */
+function divFunction($arg1 = 0, $arg2 = 1)
+{
+    if ($arg2 == 0) {
+        return (string)'ошибка, на 0 делить нельзя!';
     }
-    // Функция вычитания
-    function subFunction($arg1 = 0, $arg2 = 0)
-    {
-        return (string)($arg1 - $arg2);
-    }
-    // Функция умножения
-    function mulFunction($arg1 = 0, $arg2 = 0)
-    {
-        return (string)($arg1 * $arg2);
-    }
-    // Функция деления
-    function divFunction($arg1 = 0, $arg2 = 1)
-    {
-        if ($arg2 == 0) {
-            return (string)'ошибка, на 0 делить нельзя!';
-        }
-        return (string)($arg1 / $arg2);
-    }
+    return (string)($arg1 / $arg2);
+}
