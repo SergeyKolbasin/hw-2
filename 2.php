c3e631e0fe129e566a3e0f0ae00821f7ddc57763
jetsaus <jetsaus@gmail.com>
2022-03-28 21:42:48 +0800

    2.Доработан п.2 ДЗ в части switch...case

--- 2.php
+++ 2.php
@@ -1,42 +1,43 @@
 <?php
     $title = 'Условия';
     $h4 = 'Оператор case:';
-    $a = random_int(0, 15);                    // псевдослучайное число
+    $a = random_int(0, 15);                 // псевдослучайное число
     $strResult = '';                        // результат вычисления
-        switch ($a) {
-            case 0:
-                $strResult .= '0 ';
-            case 1:
-                $strResult .= '1 ';
-            case 2:
-                $strResult .= '2 ';
-            case 3:
-                $strResult .= '3 ';
-            case 4:
-                $strResult .= '4 ';
-            case 5:
-                $strResult .= '5 ';
-            case 6:
-                $strResult .= '6 ';
-            case 7:
-                $strResult .= '7 ';
-            case 8:
-                $strResult .= '8 ';
-            case 9:
-                $strResult .= '9 ';
-            case 10:
-                $strResult .= '10 ';
-            case 11:
-                $strResult .= '11 ';
-            case 12:
-                $strResult .= '12 ';
-            case 13:
-                $strResult .= '13 ';
-            case 14 :
-                $strResult .= '14 ';
-            case 15:
-                $strResult .= '15';
-        }
+    // Обработка оператором switch...case
+    switch ($a) {
+        case 0:
+            $strResult .= '0 ';
+        case 1:
+            $strResult .= '1 ';
+        case 2:
+            $strResult .= '2 ';
+        case 3:
+            $strResult .= '3 ';
+        case 4:
+            $strResult .= '4 ';
+        case 5:
+            $strResult .= '5 ';
+        case 6:
+            $strResult .= '6 ';
+        case 7:
+            $strResult .= '7 ';
+        case 8:
+            $strResult .= '8 ';
+        case 9:
+            $strResult .= '9 ';
+        case 10:
+            $strResult .= '10 ';
+        case 11:
+            $strResult .= '11 ';
+        case 12:
+            $strResult .= '12 ';
+        case 13:
+            $strResult .= '13 ';
+        case 14 :
+            $strResult .= '14 ';
+        case 15:
+            $strResult .= '15';
+    }
 ?>
 <html lang="ru">
 <head>
